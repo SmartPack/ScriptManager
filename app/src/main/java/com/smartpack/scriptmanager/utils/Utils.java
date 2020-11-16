52bf60a9d07f5ae04a9f88f313a9c16ea27cd06f
sunilpaulmathew <sunil.kde@gmail.com>
2020-11-16 21:02:26 +0100

    Fixed import scripts not working in the case of restricted locations
    Signed-off-by: sunilpaulmathew <sunil.kde@gmail.com>

--- app/src/main/java/com/smartpack/scriptmanager/utils/Utils.java
+++ app/src/main/java/com/smartpack/scriptmanager/utils/Utils.java
@@ -282,23 +282,27 @@ public class Utils {
     }
 
     public static String readFile(String file) {
-        BufferedReader buf = null;
-        try {
-            buf = new BufferedReader(new FileReader(file));
-
-            StringBuilder stringBuilder = new StringBuilder();
-            String line;
-            while ((line = buf.readLine()) != null) {
-                stringBuilder.append(line).append("\n");
-            }
-
-            return stringBuilder.toString().trim();
-        } catch (IOException ignored) {
-        } finally {
+        if (!file.startsWith("/storage/")) {
+            return runAndGetOutput("cat '" + file + "'");
+        } else {
+            BufferedReader buf = null;
             try {
-                if (buf != null) buf.close();
-            } catch (IOException e) {
-                e.printStackTrace();
+                buf = new BufferedReader(new FileReader(file));
+
+                StringBuilder stringBuilder = new StringBuilder();
+                String line;
+                while ((line = buf.readLine()) != null) {
+                    stringBuilder.append(line).append("\n");
+                }
+
+                return stringBuilder.toString().trim();
+            } catch (IOException ignored) {
+            } finally {
+                try {
+                    if (buf != null) buf.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         }
         return null;
